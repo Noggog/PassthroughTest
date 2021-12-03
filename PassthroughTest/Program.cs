87c9f3016c08a11505a5330cb93a56a6ff0018dd
Justin Swanson <justin.c.swanson@gmail.com>
2021-12-02 19:03:46 -0600

    Initial code

---
+++ PassthroughTest/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+using System.Threading.Tasks;
+
+namespace PassthroughTest
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await SynthesisPipeline.Instance
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .SetTypicalOpen(GameRelease.SkyrimSE, "PassthroughTest.esp")
+                .Run(args);
+        }
+
+        public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            foreach (var npc in state.LoadOrder.PriorityOrder.Npc().WinningOverrides())
+            {
+                state.PatchMod.Npcs.GetOrAddAsOverride(npc);
+            }
+        }
+    }
+}
